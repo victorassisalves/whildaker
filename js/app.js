e080c9e0e5194b95a94ce5ea7ae19f99fc79dc81
victorassisalves <victor.assis.alves@gmail.com>
2016-06-22 20:10:48 -0300

    Projeto Whildaker responsivo, e ajustes de dropdown

--- js/app.js
+++ js/app.js
@@ -17,25 +17,18 @@ function resizeDiv() {
     vph = $(window).height();
     $('#whildaker').css({'height': vph + 'px'});
 };
-
+ var divHeight = $('#linhaHome').css("height");
 // quando a página carregar começar o evento de desenhar as linhas na primeira section.
 function drawLine(){
-    var path = document.querySelector('.squiggle-animated path');
-    var length = path.getTotalLength();
-// Clear any previous transition
-    path.style.transition = path.style.WebkitTransition =
-        'none';
-// Set up the starting positions
-    path.style.strokeDasharray = length + ' ' + length;
-    path.style.strokeDashoffset = length;
-// Trigger a layout so styles are calculated & the browser
-// picks up the starting position before animating
-    path.getBoundingClientRect();
-// Define our transition
-    path.style.transition = path.style.WebkitTransition =
-        'stroke-dashoffset 2s ease-in-out';
-// Go!
-    path.style.strokeDashoffset = '0';
+    $(".lineH").css({
+        width: '+=50%',
+        "text-align": 'left',
+        transition: '3s'
+    });
+    $(".lineV").animate({
+        heigth: "divHeight",
+        transition: '3s'
+    });
     //quando as linha atingirem o ponto final, mostrar o menu.
 };
 
