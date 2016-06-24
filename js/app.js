5b9134658256f6d711c4bdd03d39940f24d606d8
victorassisalves <victor.assis.alves@gmail.com>
2016-06-24 16:20:59 -0300

    Projeto Whildaker: efeitos de linha home com problema no temporizador em algumas resoluções. e erro na direção das linhas na section escritório

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 window.onresize = function(event) {
     resizeDiv();
 };
-
+var sectionWidht = $("#whildaker").width();
 function resizeDiv() {
     vph = $(window).height();
     $('#whildaker').css({'height': vph + 'px'});
@@ -24,21 +24,32 @@ function resizeDiv() {
 function drawLine(){
     $(".lineH").css({
         width: '100%',
-        'transition-duration': '7.7s'
+        'transition-duration': '10s'
     });
     $(".lineH2").css({
         width: '100%',
-        'transition': '1s ease 0.72s'
+        'transition': '1s ease 0.82s'
     });
     $(".lineV").css({
         height: '85%',
+        bottom: "2rem",
         transition: '1s'
     });
+
+    //remover o top pq quebra no chrome
+    function removeTop() {
+        $(".lineV").css({top: 'auto'});
+        console.log('tamanho do top ' + $(".lineV").css("top"));
+        console.log('tamanho do bottom ' + $(".lineV").css("bottom"));
+    }
+    setTimeout(removeTop, 900);
+
     $(".lineV2").css({
         height: '85%',
         transition: '1s ease 1.72s'
     });
 
+    // Letras aparecendo uma a uma
     var string = $('.textwhildaker p').html();
     $('.textwhildaker p').html("");
     var q = jQuery.map(string.split(''), function (letter) {
@@ -57,10 +68,45 @@ function drawLine(){
 
         if (c >= q.length) clearInterval(i);
     }, 20);
+
     //quando as linha atingirem o ponto final, mostrar o menu.
 
-    $(".nodropdown").delay(2500).fadeIn();
+    console.log(sectionWidht);
+    if (sectionWidht > "767"){
+        $(".nodropdown").delay(2500).css({opacity: 0, visibility: "visible"}).animate({opacity: 1}, 'slow');
+    }
 };
 
+
 //quando o usuário rolar para a sessão de baixo, identificar a posição
+
+//escritório
+var posicaoInicial = $('#escritorio').position().top;
+$(document).scroll(function () { // oscultador de scroll
+    var posicaoScroll = $(document).scrollTop(); // obtem a quantidade de scroll no momento
+
+    //adicionar o box shadow à imagem
+    if (posicaoInicial < posicaoScroll) $('#escritorio img').css({
+    '-webkit-box-shadow': '0px 0px 8px 4px rgba(0,0,0,0.58)',
+    '-moz-box-shadow': '0px 0px 8px 4px rgba(0,0,0,0.58)',
+    'box-shadow': '0px 0px 8px 4px rgba(0,0,0,0.58)',
+    transition: 'box-shadow 1s'
+    });
+
     //Ativar o evento de desenho das linhas e aparecimento do texto
+
+    if (posicaoInicial < posicaoScroll){
+        if (sectionWidht > "767"){
+            $(".lineHEsc").css({
+                transition: '1s',
+                width: '100px'
+            });
+            $(".lineHEsc2").css({
+                transition: '1s',
+                width: '40px'
+            });
+            $(".fundobranco h1, .fundobranco p").animate({opacity: 1}, 'slow');
+        }
+
+    }
+});
